12cef252f17a62f41587ecfdad4a516e76da563c
Karol Cymerys <karol.cymerys@gmail.com>
2020-08-18 10:52:31 +0200

    init commit

---
+++ src/main/java/com/kcymerys/java/todowithcognito/TodoWithCognitoApplication.java
@@ -0,0 +1,13 @@
+package com.kcymerys.java.todowithcognito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoWithCognitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoWithCognitoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kcymerys/java/todowithcognito/TodoWithCognitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kcymerys.java.todowithcognito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoWithCognitoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
