24cb9467f41195172f6cb3e5b4e8b1ea07ecbbb5
Karol Cymerys <karol.cymerys@gmail.com>
2020-08-19 17:15:53 +0200

    Authorization

--- src/main/java/com/kcymerys/java/todowithcognito/utils/CognitoProperties.java
+++ src/main/java/com/kcymerys/java/todowithcognito/utils/CognitoProperties.java
@@ -11,5 +11,7 @@ public class CognitoProperties {
     private String clientId;
     private String clientSecret;
     private String redirectUri;
+    private String issuer;
+    private String keyStorePath;
 
 }
